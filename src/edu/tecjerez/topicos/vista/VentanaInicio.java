1e76f253dd5c0559e68412012fa0e22db874fb71
Diana Rebeca Ortega <diana.os@tecjerez>
2025-03-14 17:56:52 -0600

    Se separo la interfas de triangulo en una clase fuera de VentanaInicio

--- src/edu/tecjerez/topicos/vista/VentanaInicio.java
+++ src/edu/tecjerez/topicos/vista/VentanaInicio.java
@@ -22,22 +22,22 @@ public class VentanaInicio extends JFrame {
 	//circulo c1 = new circulo(); //No se puede usar porque el modificador de acceso es default 
 
 	triangulo t1 = new triangulo();
-	
+
 	figuras f1 = new figuras();
-	
-	
+
+
 	//Rectanguo r1 = new Rectangulo// no se puede instanciar una subclase de esta manera
 
 	//FORMA CORRECTA DE instancias una clase interna ////siiiiiiiiiiiiiiiiiiiiiiiiii esta correcto 
 	figuras.Rectangulo r1 = new figuras().new Rectangulo();
-    
+
 
 	Conicos conic = new Conicos();
 	Conicos.Circulo c1 = new Conicos().new Circulo();
-	
 
-	
-	
+
+
+
 	public VentanaInicio() {
 		getContentPane().setBackground(new Color (255, 249, 176) );
 		setSize(700, 530);
@@ -91,121 +91,61 @@ public class VentanaInicio extends JFrame {
 
 				JMenuBar menuBar = new JMenuBar();//DECLARACION DEL MENU BAR
 				menuBar.setBounds(0, 0, 830,30);
-				
-				
+
+
 				JMenu btnDosDimensiones  = new JMenu("2D");/////////////////////////////////////////////////
 				menuBar.add(btnDosDimensiones);
-				
+
 				JMenu Conicos = new JMenu ("Conicos");
 				btnDosDimensiones.add(Conicos);
-				
+
 				JMenuItem Circulo = new JMenuItem ("Circulo");
 				Conicos.add(Circulo);
-				
+
 				JMenuItem Elipse = new JMenuItem ("Elipse");
 				Conicos.add(Elipse);
-				
+
 				JMenu Poligono = new JMenu ("Poligono");
 				btnDosDimensiones.add(Poligono);
-				
+
 				JMenuItem Rombo = new JMenuItem ("Rombo");
 				Poligono.add(Rombo);
 
 				JMenu btnTresDimensiones= new JMenu("3D");///////////////////////////////////////////////
 				menuBar.add(btnTresDimensiones);
-				
+
 				JMenuItem Cono = new JMenuItem ("Cono");
 				btnTresDimensiones.add(Cono);
-				
+
 				JMenuItem Piramide = new JMenuItem ("Piramide");
 				btnTresDimensiones.add(Piramide);
 
-				 btnOtras = new JMenu("Otras");/////////////////////////////////////////////BOTON OTROS
+				btnOtras = new JMenu("Otras");/////////////////////////////////////////////BOTON OTROS
 				menuBar.add(btnOtras);
-				
+
 				Triangulo = new JMenuItem ("Triangulo");
 				btnOtras.add(Triangulo);
+				VentanaTriangulo ventanitaTriangulo = new VentanaTriangulo();
 				Triangulo.addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						
 						JPanel panelTriangulo = new JPanel();
 						panelTriangulo.setLayout(null);
 						panelTriangulo.setBounds(30,30, 600, 280);
 						panelTriangulo.setBackground(new Color(255, 193, 180 ));
-						
-						JLabel txtFormulaHeron = new JLabel ("Calculo de area de un triangulo con la formula de heron");
-						txtFormulaHeron.setBounds(30,00, 500,200);	
-						txtFormulaHeron.setFont(new Font("Georgia", Font.BOLD, 16));
-						txtFormulaHeron.setIcon(new ImageIcon("C:\\Users\\Marcelo\\eclipse-workspace\\Sesion10_Componentes y librerias\\src\\edu\\tecjerez\\topicos\\vista\\ICONOS\\Triangulo.png"));
-						txtFormulaHeron.setHorizontalTextPosition( SwingConstants.CENTER );// Texto en el centro y debajo del icono
-						txtFormulaHeron.setVerticalTextPosition( SwingConstants.BOTTOM );// Texto en el centro y debajo del icono
-						panelTriangulo.add(txtFormulaHeron);
-						
-						JLabel txtLadoA =new JLabel ("Ingresa lado A:");
-						txtLadoA.setBounds(20,170,300,20);
-						panelTriangulo.add(txtLadoA);
-						
-						JTextField cajaA = new JTextField(30);
-						cajaA.setBounds(115,170,200,20);
-						panelTriangulo.add(cajaA);
-						
-						JLabel txtLadoB =new JLabel ("Ingresa lado B:");
-						txtLadoB.setBounds(20,190,300,20);
-						panelTriangulo.add(txtLadoB);
-						
-						JTextField cajaB = new JTextField(30);
-						cajaB.setBounds(115,190,200,20);
-						panelTriangulo.add(cajaB);
-						
-						JLabel txtLadoC =new JLabel ("Ingresa lado C:");
-						txtLadoC.setBounds(20,210,300,20);
-						panelTriangulo.add(txtLadoC);
-						
-						JTextField cajaC = new JTextField(30);
-						cajaC.setBounds(115,210,200,20);
-						panelTriangulo.add(cajaC);
-						
-						JButton btnCAceptar= new JButton(" Aceptar");
-						btnCAceptar.setBounds(140,230,150,20);
-						panelTriangulo.add(btnCAceptar);
-						JLabel txtResultado =new JLabel ("Resultado:");
-						btnCAceptar.addActionListener(new ActionListener() {
-							
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								txtResultado.setText( "Resultado: "+ 
-								t1.obtenerAreaTriangulo(
-										Double.parseDouble(cajaA.getText()),
-										Double.parseDouble(cajaB.getText()),
-										Double.parseDouble(cajaC.getText()))
-										);
-								
-							}
-						});
-						
-						
-						txtResultado.setBounds(20,250,300,20);
-						panelTriangulo.add(txtResultado);
-						
-						
-
-						
-						panel1.add(panelTriangulo);
-						panel1.revalidate();//para que el panel no se abra en miniatura y se actualice el nuevo panel 
-						panel1.repaint();
+						ventanitaTriangulo.InterfasTriangulo(panelTriangulo, panel1);
 
 					}
-					
+
 				});
-					
-					
-				
-				
-				
+
+
+
+
+
 				JMenuItem Rectangulo = new JMenuItem ("Rectangulo");
 				btnOtras.add(Rectangulo);
-				
+
 				setJMenuBar(menuBar);
 				panel1.add(menuBar);
 				InternalFrame.add(panel1);
@@ -215,6 +155,6 @@ public class VentanaInicio extends JFrame {
 
 	}
 
-	
+
 
 }
--- src/edu/tecjerez/topicos/vista/VentanaTriangulo.java
+++ src/edu/tecjerez/topicos/vista/VentanaTriangulo.java
@@ -1,10 +1,91 @@
 package edu.tecjerez.topicos.vista;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import edu.tecjerez.topicos.figuras.triangulo;
+
 public class VentanaTriangulo {
+	triangulo t1 = new triangulo();
+public void InterfasTriangulo(JPanel panelTriangulo, JPanel panel1) {
+	
+	
+	
+	JLabel txtFormulaHeron = new JLabel ("Calculo de area de un triangulo con la formula de heron");
+	txtFormulaHeron.setBounds(30,00, 500,200);	
+	txtFormulaHeron.setFont(new Font("Georgia", Font.BOLD, 16));
+	txtFormulaHeron.setIcon(new ImageIcon("C:\\Users\\Marcelo\\eclipse-workspace\\Sesion10_Componentes y librerias\\src\\edu\\tecjerez\\topicos\\vista\\ICONOS\\Triangulo.png"));
+	txtFormulaHeron.setHorizontalTextPosition( SwingConstants.CENTER );// Texto en el centro y debajo del icono
+	txtFormulaHeron.setVerticalTextPosition( SwingConstants.BOTTOM );// Texto en el centro y debajo del icono
+	panelTriangulo.add(txtFormulaHeron);
+	
+	JLabel txtLadoA =new JLabel ("Ingresa lado A:");
+	txtLadoA.setBounds(20,170,300,20);
+	panelTriangulo.add(txtLadoA);
+	
+	JTextField cajaA = new JTextField(30);
+	cajaA.setBounds(115,170,200,20);
+	panelTriangulo.add(cajaA);
+	
+	JLabel txtLadoB =new JLabel ("Ingresa lado B:");
+	txtLadoB.setBounds(20,190,300,20);
+	panelTriangulo.add(txtLadoB);
+	
+	JTextField cajaB = new JTextField(30);
+	cajaB.setBounds(115,190,200,20);
+	panelTriangulo.add(cajaB);
+	
+	JLabel txtLadoC =new JLabel ("Ingresa lado C:");
+	txtLadoC.setBounds(20,210,300,20);
+	panelTriangulo.add(txtLadoC);
+	
+	JTextField cajaC = new JTextField(30);
+	cajaC.setBounds(115,210,200,20);
+	panelTriangulo.add(cajaC);
+	
+	JButton btnCAceptar= new JButton(" Aceptar");
+	btnCAceptar.setBounds(140,230,150,20);
+	panelTriangulo.add(btnCAceptar);
+	JLabel txtResultado =new JLabel ("Resultado:");
+	btnCAceptar.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			txtResultado.setText( "Resultado: "+ 
+			t1.obtenerAreaTriangulo(
+					Double.parseDouble(cajaA.getText()),
+					Double.parseDouble(cajaB.getText()),
+					Double.parseDouble(cajaC.getText()))
+					);
+			
+		}
+	});
+	
+	
+	txtResultado.setBounds(20,250,300,20);
+	panelTriangulo.add(txtResultado);
+	
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
+	panel1.add(panelTriangulo);
+	panel1.revalidate();//para que el panel no se abra en miniatura y se actualice el nuevo panel 
+	panel1.repaint();
+	
+	
 }
+	
+	
+}
+	
+	
+
